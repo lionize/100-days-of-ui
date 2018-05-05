6fb61f172cbebbbc2f218096a0c580c9fc86b16d
Mark Chandler <mark@chanchan.io>
2018-05-05 15:35:32 -0400

    feat(style): Change colorscheme to purple

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Registration from 'components/registration'
 import Header from 'components/Header'
 
 class App extends Component {
@@ -7,7 +8,9 @@ class App extends Component {
       <div className="container w-screen h-screen p-4 m-auto">
         <Header />
         <main className="w-full my-8 ">
-          <div className="p-3 w-full bg-white rounded border-blue-dark border-t-4 border-solid shadow-lg" />
+          <div className="p-4 w-full bg-white rounded border-purple-light border-t-4 border-solid shadow-lg">
+            <Registration />
+          </div>
         </main>
       </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Logo from 'components/Logo'
 
 const Header = () => (
-  <header className="flex items-center border-b border-solid border-blue-darker pb-2">
+  <header className="flex items-center border-b border-solid border-purple-darker pb-2">
     <Logo animate={true} size="48px" />
-    <h1 className="ml-2 font-semibold text-xxl text-blue-darker">Registratr</h1>
+    <h1 className="ml-2 font-semibold text-xxl text-purple-darker">
+      Registratr
+    </h1>
   </header>
 )
 
