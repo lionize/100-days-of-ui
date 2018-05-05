d8dc9bddb10cb155577a46abf8ed3bc48ed8428a
Mark Chandler <mark@chanchan.io>
2018-05-05 12:32:11 -0400

    feat(machine): Create xstate machine components

---
+++ src/components/machine/FiniteMachine.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import {Machine} from 'xstate'
+import PropTypes from 'prop-types'
+
+class FiniteMachine extends React.Component {
+  static propTypes = {
+    log: PropTypes.bool,
+    chart: PropTypes.object.isRequired,
+    reducer: PropTypes.func.isRequired,
+    render: PropTypes.func.isRequired,
+  }
+
+  machine = Machine(this.props.chart)
+
+  state = {
+    data: this.props.reducer(undefined, {type: '@init'}),
+    machineState: this.machine.config.initial,
+  }
+
+  transition = (type, newData) => {
+    const {log, chart, reducer} = this.props
+    const {data, machineState} = this.state
+    const nextState = this.machine.transition(machineState, type).value
+    const action = {
+      data: newData,
+      type: `${machineState}.${type}`,
+    }
+
+    if (log) {
+      console.log(chart.id, action.type, action.data)
+    }
+
+    this.setState(
+      () => ({
+        data: reducer(data, action),
+        machineState: nextState,
+      }),
+      log &&
+        (() => {
+          console.log(chart.id, this.state.machineState, this.state.data)
+        }),
+    )
+  }
+
+  render() {
+    return this.props.render({
+      state: this.state.machineState,
+      data: this.state.data,
+      transition: this.transition,
+    })
+  }
+}
+
+export default FiniteMachine
---
+++ src/components/machine/Switch.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Switch extends React.Component {
+  static propTypes = {
+    machine: PropTypes.shape({
+      state: PropTypes.string.isRequired,
+      transition: PropTypes.func.isRequired,
+    }).isRequired,
+  }
+
+  render() {
+    const {children, machine} = this.props
+
+    let match = null
+
+    React.Children.forEach(children, child => {
+      if (match) return
+      if (child.props.state === machine.state) {
+        match = child
+      }
+    })
+
+    return React.cloneElement(match, {machine})
+  }
+}
+
+export default Switch
